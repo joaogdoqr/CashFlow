2398eade0611f757c58306f5322a055cecd9fd7e
João Pedro Giacometti Guerreiro <joao.guerreiro@doqr.com.br>
2024-09-16 11:32:43 -0300

    feat: add db config

---
+++ src/CashFlow.Domain/Entities/Expense.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Domain.Enums;
+using System;
+
+namespace CashFlow.Domain.Entities
+{
+    public class Expense
+    {
+        public long Id { get; set; }
+
+        public required string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public PaymentType PaymentType { get; set; }
+    }
+}
